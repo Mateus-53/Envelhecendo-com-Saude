1b3e7c54b8dd1f8820a281796c231e2b86228025
luiz-developer <luizandreleal100@gmail.com>
2023-06-11 14:22:49 -0300

    Barrinha que aumenta ao dar scroll

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+alert(1)
+window.onscroll = function () {
+   updateProgressBar();
+};
+
+function updateProgressBar() {
+   var scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+   var documentHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+   var progress = (scrollTop / documentHeight) * 100;
+   document.querySelector(".scrollIndex").style.width = progress + "%";
+}
