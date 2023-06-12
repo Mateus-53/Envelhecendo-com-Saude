949f579f7cf00dbba44df5ea002f8ee60a679c20
luiz-developer <luizandreleal100@gmail.com>
2023-06-12 17:29:48 -0300

    Error do scrollIndex corrigido

--- app/js/menuLateral.js
+++ app/js/menuLateral.js
@@ -2,11 +2,13 @@ const btnMenu = document.querySelector(".openMenu");
 const lateralNav = document.querySelector(".lateralNav");
 const overlay = document.querySelector(".overlay");
 const header = document.querySelector(".header");
+const scrollIndex = document.querySelector(".scrollIndex");
 
 btnMenu.addEventListener("click", function () {
    lateralNav.classList.toggle("opened");
    overlay.classList.toggle("show");
    header.classList.toggle("opened");
+   scrollIndex.classList.toggle("opened");
 
    console.log(overlay);
 });
